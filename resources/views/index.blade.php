ddd010a7f79e867b27c5292fa8d0bd1aefd9407a
isbero09 <isme09mocha@gmail.com>
2025-12-25 00:22:10 -0500

    Al normal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,16 +35,7 @@
                     <li>
                         <hr class="dropdown-divider" />
                     </li>
-                    <li>
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            Logout
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </li>
+                    <li><a class="dropdown-item" href="index.html">Logout</a></li>
                 </ul>
             </li>
         </ul>
